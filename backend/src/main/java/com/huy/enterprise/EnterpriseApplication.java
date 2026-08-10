74dc6b6b7b6418a86f730ca99fe9fa3f2b4b0420
HuyDEV10 <nddhuy11b1@gmail.com>
2026-08-10 15:30:39 +0700

    hoàn thành giai đoạn 1

--- backend/src/main/java/com/huy/enterprise/EnterpriseApplication.java
+++ backend/src/main/java/com/huy/enterprise/EnterpriseApplication.java
@@ -1,12 +1,17 @@
 package com.huy.enterprise;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EnterpriseApplication {
 
+    private static final String APPLICATION_TIME_ZONE = "Asia/Ho_Chi_Minh";
+
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone(APPLICATION_TIME_ZONE));
         SpringApplication.run(EnterpriseApplication.class, args);
     }
 }
