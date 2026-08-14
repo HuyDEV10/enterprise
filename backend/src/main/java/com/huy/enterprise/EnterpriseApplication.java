9a37ea215b10dd60076cf843daacbfd6d4cca276
HuyDEV10 <nddhuy11b1@gmail.com>
2026-08-14 18:54:38 +0700

    done phase 3

--- backend/src/main/java/com/huy/enterprise/EnterpriseApplication.java
+++ backend/src/main/java/com/huy/enterprise/EnterpriseApplication.java
@@ -1,5 +1,6 @@
 package com.huy.enterprise;
 
+import java.time.ZoneId;
 import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
@@ -10,8 +11,13 @@ public class EnterpriseApplication {
 
     private static final String APPLICATION_TIME_ZONE = "Asia/Ho_Chi_Minh";
 
+    static {
+        System.setProperty("user.timezone", APPLICATION_TIME_ZONE);
+        TimeZone.setDefault(
+                TimeZone.getTimeZone(ZoneId.of(APPLICATION_TIME_ZONE)));
+    }
+
     public static void main(String[] args) {
-        TimeZone.setDefault(TimeZone.getTimeZone(APPLICATION_TIME_ZONE));
         SpringApplication.run(EnterpriseApplication.class, args);
     }
 }
